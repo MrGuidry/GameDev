00aa5bb13df2eb6839d4a90160843de1a82c6d38
Katie Cox <14coxkm@gmail.com>
2020-03-23 21:16:00 -0700

    tripleshot works

--- Galaga/scripts/Player.gd
+++ Galaga/scripts/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var speed = Vector2(0,0)
 var movement = 400
+var powerTriple = true
 
 var projectile = preload("res://Projectile.tscn")
 
@@ -22,11 +23,37 @@ func get_input():
 		if(position.x < 270):
 			speed.x = movement
 	if Input.is_action_just_pressed("Spacebar"):
-		if(get_tree().get_nodes_in_group("Bullets").size() < 4):
+		if(!powerTriple && get_tree().get_nodes_in_group("Bullets").size() < 4):
+			shoot()
+		elif(powerTriple && get_tree().get_nodes_in_group("Bullets").size() < 12):
 			shoot()
 	
 func shoot():
-	var bullet = projectile.instance()
-	bullet.position = get_position()
-	bullet.position.y -= 45
-	get_parent().add_child(bullet)
+	if(powerTriple):
+		var bullet1 = projectile.instance()
+		var bullet2 = projectile.instance()
+		var bullet3 = projectile.instance()
+		
+		bullet1.position = get_position()
+		bullet2.position = get_position()
+		bullet3.position = get_position()
+	
+		bullet1.position.y -= 45
+		bullet2.position.y -= 45
+		bullet3.position.y -= 45
+		
+		
+		bullet1.position.x -= 50
+		bullet1.leftShot()
+		
+		bullet3.position.x += 50
+		bullet3.rightShot()
+		
+		get_parent().add_child(bullet1)
+		get_parent().add_child(bullet2)
+		get_parent().add_child(bullet3)
+	else:
+		var bullet = projectile.instance()
+		bullet.position = get_position()
+		bullet.position.y -= 45
+		get_parent().add_child(bullet)
--- Galaga/scripts/Projectile.gd
+++ Galaga/scripts/Projectile.gd
@@ -10,3 +10,9 @@ func _physics_process(delta):
 	if(position.y < -700):
 		queue_free()
 	move_and_slide(velocity)
+
+func leftShot():
+	velocity.x = -50
+
+func rightShot():
+	velocity.x = 50
